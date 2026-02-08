825f60b28c558ac75c87a6ccf4ab0098875d23eb
Дмитрий <starikov.dmitriy@yandex.ru>
2026-02-08 16:04:03 +0300

    fix repo

---
+++ inc/ds18.h
@@ -0,0 +1,26 @@
+#ifndef DS18_H_
+#define DS18_H_
+
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "main.h"
+
+#define NOID 0xCC //Пропустить идентификацию
+#define CONVERT 0x44 //Старт измерения температуры
+#define READ_DATA 0xBE //Передача байтов ведущему
+
+
+#define PA0_INPUT 	(GPIOA->MODER &= ~(0x03)) // PA0 input 
+#define PA0_OUTPUT 	(GPIOA->MODER = (GPIOA->MODER & ~(0x03))| 0x01) // PA0 output 
+
+void ds18_init();
+uint8_t ds18_search();
+uint8_t ds18_read();
+void ds18_send(uint8_t data);
+int16_t ds18_get();
+
+
+#endif /* DS18_H_ */
