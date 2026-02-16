98b37158db31f71c82e28b40592e25211264b70a
Дмитрий <starikov.dmitriy@yandex.ru>
2026-02-16 21:46:47 +0300

    Create flash.sh

---
+++ flash.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+####### Настройки ###########
+interfaceCfg="interface/stlink.cfg"
+targetCfg="target/stm32f1x.cfg"
+firmware="build/main.hex"
+#############################
+
+openocd -f "$interfaceCfg" -f "$targetCfg" &
+sleep 3
+
+# telnet
+telnet 127.0.0.1 4444 << EOF
+reset halt
+flash write_image erase $firmware
+reset run
+exit
+EOF
+
+# Сообщение о завершении
+echo -e "\e[32m Прошивка загружена! \e[0m"
+sleep 3
